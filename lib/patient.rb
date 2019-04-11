40f44281a47ce92d1c9ab082b5779c9ddb4d71d6
James Cox <jamesncox@yahoo.com>
2019-04-11 15:36:05 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -14,9 +14,9 @@ class Patient
     @@all
   end
   
-  def new_appointment(doctor:, date:)
+  def new_appointment(doctor, date)
     # binding.pry
-    Appointment.new(self, doctor, date)
+    Appointment.new(self, date, doctor)
   end
   
   def appointments
